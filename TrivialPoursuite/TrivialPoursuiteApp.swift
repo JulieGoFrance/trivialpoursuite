f4f25747a692afe58b7f5c487aca59bcd6d49952
Julie Go <julie.gosselin84@yahoo.fr>
2022-09-14 20:58:45 +0200

    Initial Commit

---
+++ TrivialPoursuite/TrivialPoursuiteApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrivialPoursuiteApp.swift
+//  TrivialPoursuite
+//
+//  Created by User on 14/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct TrivialPoursuiteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
